df45efb0f48ea01b5e10b375d5514503ebdae205
thibaultlaugel <thibaultlaugel@users.noreply.github.com>
2021-01-26 18:07:22 +0100

    Adding dataset
    Boston, Credit Card and Churn

--- discrepancies/datasets.py
+++ discrepancies/datasets.py
@@ -58,6 +58,31 @@ def get_dataset(dataset='half-moons',
         y = data.target
         feature_names = data.feature_names
         target_names = data.target_names
+        
+    elif dataset == 'boston':
+        data = fetch_openml(data_id=853, return_X_y=False)
+        X = data.data
+        y = data.target
+        y = (y=='P').astype('int')
+        feature_names = data.feature_names
+        target_names = data.target_names
+    
+    elif dataset == 'credit-card': # Warning: 1) Huge dataset. 2) Etremely Imbalanced: Accuracy not appropriate, use Recall/F1
+        data = fetch_openml(data_id=1597, return_X_y=False)
+        X = data.data
+        y = data.target.astype('int')
+        feature_names = data.feature_names
+        target_names = data.target_names
+   
+    elif dataset == 'churn': # Warning: Imbalanced! Accuracy not appropriate, use Recall/F1
+        data = fetch_openml(data_id=40701, return_X_y=False)
+        df = pd.DataFrame(data.data, columns=data.feature_names)
+        del df['area_code'] #drop categorical attribute
+        X = df.values
+        feature_names = df.columns
+        y = data.target.astype('int')
+        target_names = data.target_names
+
 
     else:
         raise ValueError
